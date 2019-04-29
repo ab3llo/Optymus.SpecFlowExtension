9414de33dd535e7a391b81709b731bfaaa7e9edb
Antonio <ab3llo@outlook.com>
2019-04-29 19:29:35 +0100

    Adde specflow packagae

--- SpecFlowExtension/FrameworkHooks.cs
+++ SpecFlowExtension/FrameworkHooks.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using TechTalk.SpecFlow;
 
-namespace SpecFlowExtension
+namespace SpecFlowExtension.FrameworkHooks
 {
-	public class Class1
+	public class FrameworkHooks
 	{
+		private readonly ScenarioContext _scenarioContext;
+		private readonly FeatureContext _featureContext;
+
+		public FrameworkHooks(ScenarioContext scenarioContext, FeatureContext featureContext)
+		{
+			_scenarioContext = scenarioContext;
+			_featureContext = featureContext;
+		}
+
+		[BeforeFeature(Order = 0)]
+		public void BeforeFeature()
+		{
+
+		}
+
+		[BeforeScenario(Order = 0)]
+		public void BeforeTest()
+		{
+
+		}
 	}
 }
