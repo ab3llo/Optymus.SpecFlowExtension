7c4c27826dbdb2d5e2e92c6e2fa0b19541cef161
Antonio Bello <ab3llo@outlook.com>
2019-05-04 18:26:51 +0100

    Added Data Context

---
+++ src/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Optymus.SpecFlowExtension
+{
+	public class DataContext : IDataContext
+	{
+		public Dictionary<string, object> Current { get; set; }
+
+		public DataContext()
+		{
+			Current = new Dictionary<string, object>();
+		}
+	}
+}
---
+++ src/IDataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Optymus.SpecFlowExtension
+{
+	public interface IDataContext
+	{
+		Dictionary<string, object> Current { get; set; }
+	}
+}
